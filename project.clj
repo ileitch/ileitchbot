e861993f66af47d1e6d6f6acf07f43e1bc790615
Ian Leitch <port001@gmail.com>
2012-07-14 19:22:48 -0700

    Bump to clojure 1.4

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject ileitchbot "1.0.0-SNAPSHOT"
   :main ileitchbot.core
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [clojure-twitter "1.2.6-SNAPSHOT"]
                  [overtone/at-at "1.0.0"]
                  [http.async.client "0.4.5"]
